9292536e9bc2f2118ec55547de8a25b19316b87c
Noé Falzon <noe@yousician.com>
2020-02-01 23:49:40 +0200

    Button scaling on hover

---
+++ fgj2020/Assets/Daytime/ScaleOnHover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScaleOnHover : MonoBehaviour
+{
+    public void OnMouseOver()
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+
+    public void OnMouseExit()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
