c61dd64addc4e75c3b5fd924fcacc848640f5309
Noé Falzon <noe@yousician.com>
2020-02-01 18:30:29 +0200

    Flipping pages of notebook, populating with clues (dummies)

--- fgj2020/Assets/Daytime/OpenNotebook.cs
+++ fgj2020/Assets/Daytime/OpenNotebook.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenNotebook : MonoBehaviour
 {
     public GameObject LeftArrow;
     public GameObject RightArrow;
 
+    public GameObject Container;
+    public GameObject ClueTextPrefab;
+
     public GameObject NotebookCover;
 
+    public int CluesPerSpread = 12;
+
     private int currentPage;
+    private List<string> clues;
 
     public void Open()
     {
         gameObject.SetActive(true);
         currentPage = 0;
+        Populate(new List<string>());
     }
 
     public void FlipPage(int direction)
@@ -25,11 +32,46 @@ public class OpenNotebook : MonoBehaviour
         {
             Close();
         }
+        else
+        {
+            UpdateCluesOnPage();
+        }
     }
 
     private void Populate(List<string> clues)
     {
+        this.clues = clues;
 
+        clues.Clear();
+
+        for (int i = 0; i < 20; i++)
+            clues.Add("This is a super clue. With it you'll figure out why your life sucks so much. It's sad really. But true. " + i);
+
+        UpdateCluesOnPage();
+    }
+
+    private void UpdateCluesOnPage()
+    {
+        var startIndex = currentPage * CluesPerSpread;
+        var endIndex = Mathf.Min((currentPage + 1) * CluesPerSpread, clues.Count) - 1;
+
+        foreach (Transform child in Container.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for(int i = startIndex; i <= endIndex; i++)
+        {
+            var clue = Instantiate(ClueTextPrefab);
+            clue.transform.SetParent(Container.transform);
+            clue.transform.localScale = Vector3.one;
+
+            var textComp = clue.GetComponentInChildren<Text>();
+            textComp.text = clues[i];
+            textComp.alignment = (i - startIndex <= 5) ? TextAnchor.MiddleRight : TextAnchor.MiddleLeft;
+        }
+
+        RightArrow.SetActive(endIndex < clues.Count - 1);
     }
 
     private void Close()
