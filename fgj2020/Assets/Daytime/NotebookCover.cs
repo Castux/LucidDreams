257c9caa7b03e1c4cb8085f3885f41c43b7cd0b1
Noé Falzon <noe@yousician.com>
2020-02-01 15:03:45 +0200

    Closed to open notebook

---
+++ fgj2020/Assets/Daytime/NotebookCover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotebookCover : MonoBehaviour
+{
+    public GameObject OpenNotebook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenBook()
+    {
+        Debug.Log("Openbook");
+        OpenNotebook.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
