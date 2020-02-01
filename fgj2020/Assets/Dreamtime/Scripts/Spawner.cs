fe7aadd31177d4aa91d7120b99f2aedd73291fdb
Noé Falzon <noe@yousician.com>
2020-02-01 21:11:31 +0200

    Respawn when falling

---
+++ fgj2020/Assets/Dreamtime/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public GameObject FallThreshold;
+
+    public RotateWorldAroundPlayer worldRotater;
+
+    private GameObject currentPlayer;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        if(currentPlayer != null)
+        {
+            DestroyImmediate(currentPlayer);
+        }
+
+        currentPlayer = Instantiate(PlayerPrefab);
+        currentPlayer.transform.position = transform.position;
+
+        worldRotater.player = currentPlayer.transform;
+    }
+
+    private void Update()
+    {
+        if (currentPlayer != null && currentPlayer.transform.position.y <= FallThreshold.transform.position.y)
+        {
+            Spawn();
+        }
+    }
+}
