5c8ee968c1cb3eb6f7771e0738bfe42a8dcff5b9
Unknown <jimi.jokela@metropolia.fi>
2020-02-02 15:47:37 +0200

    Added floating cubes to dream world

---
+++ fgj2020/Assets/Dreamtime/Scripts/FloatToRandomDirection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatToRandomDirection : MonoBehaviour
+{
+    public float maxSpeed;
+    public float minSpeed;
+
+    Vector3 randomDirection;
+    float randomSpeed;
+
+    private void Start()
+    {
+        randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        randomSpeed = Random.Range(minSpeed, maxSpeed);
+
+        GetComponentInChildren<Animator>().speed = randomSpeed;
+    }
+
+    private void Update()
+    {
+        transform.position += randomDirection * Time.deltaTime * randomSpeed;
+    }
+}
---
+++ fgj2020/Assets/Dreamtime/Scripts/SpawnFloatyCubes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFloatyCubes : MonoBehaviour
+{
+    public Transform levelParent;
+    public GameObject floatyPrefab;
+    public int minFloaties;
+    public int maxFloaties;
+
+    public float minScale;
+    public float maxScale;
+
+    public void SpawnFloaties(float cylinderRadius, float levelLenght)
+    {
+        int randomCount = Random.Range(minFloaties, maxFloaties + 1);
+
+        for (int i = 0; i < randomCount; i++)
+        {
+            GameObject floaty = Instantiate(floatyPrefab, levelParent);
+            floaty.transform.position = new Vector3(Random.Range(-1f, cylinderRadius * 2f + 1f), Random.Range(-1f, cylinderRadius * 2f + 1f), Random.Range(-levelLenght, levelLenght));
+
+            float randomScale = Random.Range(minScale, maxScale);
+            floaty.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        }
+    }
+}
