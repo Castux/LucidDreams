b4a388a9edcfcea4637136b93827f81a114ae37e
Unknown <jimi.jokela@metropolia.fi>
2020-02-02 16:50:32 +0200

    Changed glitch to smooth quick fade

--- fgj2020/Assets/TitleScreen/Glitcher.cs
+++ fgj2020/Assets/TitleScreen/Glitcher.cs
@@ -12,6 +12,11 @@ public class Glitcher : MonoBehaviour
 
     private Image image;
 
+    private bool fadingToDark;
+    private bool fadingToLight;
+    private float fadeTimer;
+    public float fadeTime;
+
     private void Start()
     {
         image = GetComponent<Image>();
@@ -19,34 +24,90 @@ public class Glitcher : MonoBehaviour
 
     void Update()
     {
-        if(Random.Range(0f, 1f) < Probability)
+        if (fadingToDark)
         {
-            transform.localScale = Vector3.one + Vector3.one * Random.Range(-ScaleRange, ScaleRange);
-        }
+            if (fadeTimer < fadeTime)
+            {
+                fadeTimer += Time.deltaTime;
+                var group = GetComponent<CanvasGroup>();
+                group.alpha = Mathf.Lerp(1f, 0f, fadeTimer / fadeTime);
 
-        if (Random.Range(0f, 1f) < Probability)
-        {
-            if (UseCanvasGroup)
+            }
+            else
             {
                 var group = GetComponent<CanvasGroup>();
-                group.alpha = (group.alpha == 1f) ? 0f : 1f;
+                group.alpha = 0f;
 
                 if (group.alpha == 0f)
                     Probability *= GlitchProbFactor;
                 else
                     Probability /= GlitchProbFactor;
+
+                fadeTimer = 0f;
+                fadingToDark = false;
+            }
+        }
+        else if (fadingToLight)
+        {
+            if (fadeTimer < fadeTime)
+            {
+                fadeTimer += Time.deltaTime;
+                var group = GetComponent<CanvasGroup>();
+                group.alpha = Mathf.Lerp(0f, 1f, fadeTimer / fadeTime);
+
             }
             else
             {
-                var nextColor = Color.white;
-                nextColor.a = (image.color.a == 1f) ? 0f : 1f;
+                var group = GetComponent<CanvasGroup>();
+                group.alpha = 1f;
 
-                image.color = nextColor;
-
-                if (nextColor.a == 0f)
+                if (group.alpha == 0f)
                     Probability *= GlitchProbFactor;
                 else
                     Probability /= GlitchProbFactor;
+
+                fadeTimer = 0f;
+                fadingToLight = false;
+            }
+        }
+        else
+        {
+            if (Random.Range(0f, 1f) < Probability)
+            {
+                transform.localScale = Vector3.one + Vector3.one * Random.Range(-ScaleRange, ScaleRange);
+            }
+
+            if (Random.Range(0f, 1f) < Probability)
+            {
+                if (UseCanvasGroup)
+                {
+                    var group = GetComponent<CanvasGroup>();
+                    if (group.alpha == 0f)
+                    {
+                        fadingToLight = true;
+                    }
+                    else
+                    {
+                        fadingToDark = true;
+                    }
+
+                    // if (group.alpha == 0f)
+                    //     Probability *= GlitchProbFactor;
+                    // else
+                    //     Probability /= GlitchProbFactor;
+                }
+                else
+                {
+                    var nextColor = Color.white;
+                    nextColor.a = (image.color.a == 1f) ? 0f : 1f;
+
+                    image.color = nextColor;
+
+                    if (nextColor.a == 0f)
+                        Probability *= GlitchProbFactor;
+                    else
+                        Probability /= GlitchProbFactor;
+                }
             }
         }
     }
