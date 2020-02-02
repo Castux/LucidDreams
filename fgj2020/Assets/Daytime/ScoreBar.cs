174a60c7e46c8af843def768976bface74070a9a
Noé Falzon <noe@yousician.com>
2020-02-02 11:10:24 +0200

    Animated score bar

--- fgj2020/Assets/Daytime/ScoreBar.cs
+++ fgj2020/Assets/Daytime/ScoreBar.cs
@@ -3,25 +3,33 @@
 public class ScoreBar : MonoBehaviour
 {
     public GameObject Star;
+    public float Speed = 5f;
 
     private PlayerProgression playerProg;
+
+    private float currentProgress = 0f;
 
     void Start()
     {
         playerProg = FindObjectOfType<PlayerProgression>();
+
+        if (playerProg != null)
+            currentProgress = (float)playerProg.TotalPoints / PlayerProgression.MaxPoints;
     }
 
     void Update()
     {
-        var progress = 0f;
+        var targetProgress = 0f;
 
         if (playerProg != null)
-            progress = (float)playerProg.TotalPoints / PlayerProgression.MaxPoints;
+            targetProgress = (float)playerProg.TotalPoints / PlayerProgression.MaxPoints;
+
+        currentProgress += (targetProgress - currentProgress) * Speed * Time.deltaTime;
 
         var rect = Star.GetComponent<RectTransform>();
 
-        rect.anchorMin = new Vector2(0.5f, progress);
-        rect.anchorMax = new Vector2(0.5f, progress);
+        rect.anchorMin = new Vector2(0.5f, currentProgress);
+        rect.anchorMax = new Vector2(0.5f, currentProgress);
         rect.anchoredPosition = new Vector2(0f, 0f);
 
         GetComponent<RectTransform>().ForceUpdateRectTransforms();
