3adb6fbe97b85efe75b9bd0e6df0f884191396e3
Noé Falzon <noe@yousician.com>
2020-02-02 10:30:42 +0200

    Title screen and credits

---
+++ fgj2020/Assets/TitleScreen/TitleScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public FadeOut fader;
+    public FadeOut creditsFader;
+
+    public void OnPlayClicked()
+    {
+        fader.StartFadeOut(FadeOut.Direction.FadeOut, () =>
+        {
+            SceneManager.LoadScene("DreamWorld");
+        });
+    }
+
+    public void OnCreditsClicked()
+    {
+        fader.StartFadeOut(FadeOut.Direction.FadeOut, () =>
+        {
+            creditsFader.gameObject.SetActive(true);
+            creditsFader.StartFadeOut(FadeOut.Direction.FadeIn);
+        });
+    }
+
+    public void OnQuitClicked()
+    {
+        fader.StartFadeOut(FadeOut.Direction.FadeOut, () =>
+        {
+            Application.Quit();
+        });
+    }
+
+    public void OnBackFromCreditsClicked()
+    {
+        creditsFader.StartFadeOut(FadeOut.Direction.FadeOut, () =>
+        {
+            fader.gameObject.SetActive(true);
+            fader.StartFadeOut(FadeOut.Direction.FadeIn);
+        });
+    }
+}
